bc053f4fe8b561a649504af86271a7ddf6ea4002
Mauricio Souza Lima <mauricio.lima@corp.globo.com>
2014-09-30 10:39:31 -0300

    export Fibonacci timer

--- timer.go
+++ timer.go
@@ -2,35 +2,47 @@ package counter
 
 import "time"
 
-type fibTimer struct {
+// FibTimer will sleep in a fibonnaci sequence time at max sequence index until reset is called or stop
+// f := NewFibTimer(5, time.Second)
+// start := time.Now()
+// for t := range f.Timer() {
+//   1, 2, 3, 5, 8, 8, 8 seconds...
+// }
+// After a Reset, the timer go back to 1
+type FibTimer struct {
 	i          int
 	max        int
 	resolution time.Duration
 	evis       bool
-	C          chan time.Time
+	c          chan time.Time
 }
 
-func newFibTimer(max int, resolution time.Duration) *fibTimer {
-	f := &fibTimer{1, max, resolution, false, make(chan time.Time)}
+func NewFibTimer(max int, resolution time.Duration) *FibTimer {
+	f := &FibTimer{1, max, resolution, false, make(chan time.Time)}
 	go f.Run()
 	return f
 }
 
-func (f *fibTimer) Reset() {
+func (f *FibTimer) Reset() {
 	f.i = 1
 }
 
-func (f *fibTimer) Stop() {
+func (f *FibTimer) Stop() {
 	f.evis = false
 }
 
-func (f *fibTimer) Run() {
+func (f *FibTimer) Timer() <-chan time.Time {
+	return f.c
+}
+
+func (f *FibTimer) Run() {
 	f.evis = true
 	for f.evis {
 		t := <-time.After(time.Duration(fib(f.i+1)) * f.resolution)
-		f.C <- t
+		f.c <- t
 		f.i = min(f.max, f.i+1)
 	}
+	close(f.c)
 }
 
 func min(x, y int) int {
--- timer_test.go
+++ timer_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func (s *MySuite) TestFibTimerMax(c *C) {
-	t := newFibTimer(4, time.Millisecond)
+	t := NewFibTimer(4, time.Millisecond)
 	for i := 1; i <= 3; i++ {
-		_ = <-t.C
+		_ = <-t.Timer()
 		c.Assert(t.i, Equals, i+1)
 	}
-	_ = <-t.C
+	_ = <-t.Timer()
 	c.Assert(t.i, Equals, 4)
 	t.Reset()
 	c.Assert(t.i, Equals, 1)
-	_ = <-t.C
+	_ = <-t.Timer()
 	c.Assert(t.i, Equals, 2)
 }
